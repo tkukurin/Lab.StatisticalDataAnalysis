01dfbb116a3b697f98fac18c6d98ce0bea0f01ee
Marin <vernier.mt@gmail.com>
2017-05-01 20:25:24 +0200

    Data frames

--- main.r
+++ main.r
@@ -6,16 +6,18 @@ source_eval <- function(file) source(file, print.eval = TRUE)
 source_eval('uncommon.r')
 setwd(WORK_DIR)
 
-investment_funds <- c("CROBEX", "ERSTEAdriaticEquity", "OTPMeridian20", "ZBAktiv")
+investment_funds <- c("ERSTEAdriaticEquity", "OTPMeridian20", "ZBAktiv")
 pension_funds <- c("RaiffeisenDMF", "ERSTEPlaviEXPERT", "ERSTEPlaviPROTECT")
 
 source('data_extraction.r')
 xs <- read_normalize(CSV_DATA)
 xs.log_returns <- to_log_returns_df(xs, 3:length(xs))
 
+xs.market_portfolio <- data.frame(xs$Date,
+                                  xs$InterestRate,
+                                  xs$CROBEX)
 xs.investment <- data.frame(xs$Date, 
                             xs$InterestRate, 
-                            xs$CROBEX, 
                             xs$ERSTEAdriaticEquity,
                             xs$OTPMeridian20,
                             xs$ZBAktiv)
