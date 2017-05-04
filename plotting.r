59b5f60fbe208c884116fdeb85b0f90f7e8bf47c
Marin <vernier.mt@gmail.com>
2017-05-04 21:17:38 +0200

    Sve skupa

--- plotting.r
+++ plotting.r
@@ -1,11 +1,43 @@
 library(ggplot2)
+library(grid)
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  if (is.null(layout)) {
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
 
 plot_timeseries <- function(xs, x_col, y_col) {
   return(ggplot(xs, aes(x_col, y_col)) + 
            geom_line() +
            scale_x_date() + 
            xlab("Date") + 
-           ylab("Price")) 
+           ylab("Price") +
+           geom_smooth(method='lm')) 
 }
-
 
